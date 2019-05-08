b9ef9817f9295a21d1e398d47dafdcf00886c04e
Jun Lim <jlim0069@student.monash.edu>
2019-05-09 02:17:59 +0800

    Added some extra basic features

---
+++ src/game/Wizard.java
@@ -0,0 +1,59 @@
+package game;
+
+import edu.monash.fit2099.engine.*;
+
+/**
+ * Class representing a Wizard.
+ */
+public class Wizard extends Enemy {
+
+    /**
+     * Constructor to create a Wizard as a form of an Enemy.
+     *
+     * @param name a String representing the name of the Wizard
+     */
+    public Wizard(String name){
+        super(name, 'w', 20, 5);
+        super.addItemToInventory(new Apple("Apple"));
+        for (Item item : this.getInventory()){
+            if (item instanceof Key){
+                this.removeItemFromInventory(item);
+            }
+        }
+    }
+
+    /**
+     * Wizards are capable of resurrecting sleeping enemies as Zombies if the sleeping enemy is beside the Wizard.
+     * Wizards are unable to attack and can only move around at random or skip its run.
+     *
+     * @param actions collection of possible Actions for this Actor
+     * @param map     the map containing the Actor
+     * @param display the I/O object to which messages may be written
+     * @return the Action to be performed, e.g. move the Wizard to another place
+     */
+    @Override
+    public Action playTurn(Actions actions, GameMap map, Display display) {
+        Location qLocation = map.locationOf(this);
+        actions.clear();
+        for (Exit exit : qLocation.getExits()) {
+            Location destination = exit.getDestination();
+            if (!map.isAnActorAt(destination)) {
+                for (Item item : destination.getItems()) {
+                    if (item.getDisplayChar() == '%') {
+                        Zombie zombie = new Zombie("Zombie");
+                        map.addActor(zombie, destination.x(), destination.y());
+                        destination.removeItem(item);
+                    }
+                }
+            }
+        }
+
+        super.addActions(actions, this, map);
+        for (Action action : actions) {
+            if (action instanceof AttackAction) {
+                actions.remove(action);
+            }
+        }
+        return super.playTurn(actions, map, display);
+    }
+}
